ef889e833d1d0d0f49d2f43e0e1d4248b17de4cc
mlata <michallata.dev@gmail.com>
2024-12-05 16:48:15 +0100

    feat: Add ft_strlen to header and Makefile

--- libft/libft.h
+++ libft/libft.h
@@ -6,17 +6,20 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:39:35 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/04 15:33:17 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/05 15:27:49 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stdio.h>
+# include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
