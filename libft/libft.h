36172c414b853ee1e91b9f0cb97e7166d4b949e9
mlata <michallata.dev@gmail.com>
2024-12-06 16:42:06 +0100

    feat: Add ft_memset in header

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:39:35 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/05 15:27:49 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/05 20:29:59 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+
 size_t	ft_strlen(const char *s);
+
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
