a7a53f478e4af6fab21c69320d07d722b69500ca
Michal Lata <michallata.dev@gmail.com>
2024-12-08 21:23:43 +0100

    feat: Add ft_memmove [WIP]

---
+++ libft/ft_memmove.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void   *ft_memmove(void *dst, const void *src, size_t len)
+{
+    size_t  i;
+    unsigned char   *d;
+    const unsigned char   *s;
+
+    d = (unsigned char *)d;
+    s = (unsigned char *)s;
+    i = 0;
+    while (i < len)
+    {
+        
+    }
+    return (dst);
+}
