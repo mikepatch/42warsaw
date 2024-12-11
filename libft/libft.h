3077c30443d20c5d2b0d772f5927f2b9e2a5a16b
Michal Lata <michallata.dev@gmail.com>
2024-12-11 02:49:05 +0100

    feat: Add ft_strlcpy to header

--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: michallata <michallata@student.42.fr>      +#+  +:+       +#+        */
+/*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:39:35 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/08 00:14:54 by michallata       ###   ########.fr       */
+/*   Updated: 2024/12/11 02:37:58 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,11 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 void	*ft_memset(void *s, int c, size_t n);
-void    *ft_memcpy(void *dst, const void *src, size_t n);
-void    *ft_memmove(void *dst, const void *src, size_t len);
-void    ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	ft_bzero(void *s, size_t n);
 
 #endif
