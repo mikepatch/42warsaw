9d3940b13dd3b1fe0dd3b1f919b116a1c6ca772c
Michal Lata <michallata.dev@gmail.com>
2024-12-11 01:40:31 +0100

    feat: Add ft_memmove solution

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,24 +6,40 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 21:28:42 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/08 21:28:43 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/11 01:38:13 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void   *ft_memmove(void *dst, const void *src, size_t len)
+void *ft_memmove(void *dst, const void *src, size_t len)
 {
-    size_t  i;
-    unsigned char   *d;
-    const unsigned char   *s;
+    size_t i;
+    unsigned char *d;
+    const unsigned char *s;
 
-    d = (unsigned char *)d;
-    s = (unsigned char *)s;
-    i = 0;
-    while (i < len)
+    if (dst == NULL && src == NULL)
+        return NULL;
+
+    d = (unsigned char *)dst;
+    s = (unsigned char *)src;
+    if (d < s)
     {
-        
+        i = 0;
+        while (i < len)
+        {
+            d[i] = s[i];
+            i++;
+        }
+    }
+    else
+    {
+        i = len;
+        while (i > 0)
+        {
+            d[i - 1] = s[i - 1];
+            i--;
+        }
     }
     return (dst);
 }
