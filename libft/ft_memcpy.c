a0b79513773fc0143207d55b0b9f8afee597e9c6
Michal Lata <michallata.dev@gmail.com>
2024-12-11 02:47:52 +0100

    patch: Update tabs in ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,27 +6,27 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 21:29:21 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/08 21:29:22 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/11 02:39:32 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    if (n == 0 || dst == src)
-        return (dst);
-    size_t  i;
-    unsigned char   *d;
-    const unsigned char   *s;
+	size_t					i;
+	unsigned char			*d;
+	const unsigned char		*s;
 
-    d = (unsigned char *)dst;
-    s = (unsigned char *)src;
-    i = 0;
-    while (i < n)
-    {
-        d[i] = s[i];
-        i++;
-    }
-    return (dst);
+	if (n == 0 || dst == src)
+		return (dst);
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
 }
