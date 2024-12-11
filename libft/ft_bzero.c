a4123589422a8dbbc36dcf3b360b01f4ea6d8b1b
Michal Lata <michallata.dev@gmail.com>
2024-12-11 02:47:34 +0100

    patch: Update tabs in ft_bzero

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,22 +6,22 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 21:29:13 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/08 21:29:15 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/11 02:36:53 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_bzero(void *s, size_t n) 
+void	ft_bzero(void *s, size_t n)
 {
-    size_t  i;
-    unsigned char   *unsigned_s;
+	size_t			i;
+	unsigned char	*unsigned_s;
 
-    unsigned_s = (unsigned char *)s;
-    i = 0;
-    while (i < n)
-    {
-        unsigned_s[i] = 0;
-        i++;
-    }
+	unsigned_s = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		unsigned_s[i] = 0;
+		i++;
+	}
 }
