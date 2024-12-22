41bf19160c1e8e95f5ec2d99b4b8c2b50a19e3df
Michal Lata <michallata.dev@gmail.com>
2024-12-22 21:57:37 +0100

    feat: Add ft_strncmp solution

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 21:01:49 by mlata             #+#    #+#             */
+/*   Updated: 2024/12/22 21:44:45 by mlata            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	uc1;
+	unsigned char	uc2;
+
+	i = 0;
+	while (i < n)
+	{
+		uc1 = (unsigned char)s1[i];
+		uc2 = (unsigned char)s2[i];
+		if (uc1 != uc2 || uc1 == '\0' || uc2 == '\0')
+			return (uc1 - uc2);
+		i++;
+	}
+	return (0);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:39:35 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/22 20:57:47 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/22 21:01:37 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_strchr(const char *s, int c);
 char	*strrchr(const char *s, int c);
