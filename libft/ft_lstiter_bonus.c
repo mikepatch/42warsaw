cbb56818d94f603e243e85c403ecd10e9af5ae82
Michal Lata <michallata.dev@gmail.com>
2024-12-28 23:00:34 +0100

    patch: Update ft_lstiter_bonus

--- libft/ft_lstiter_bonus.c
+++ libft/ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 22:51:26 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/28 22:55:48 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/28 23:00:18 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	t_list	*current;
 
+	if (!lst || !f)
+		return ;
 	current = lst;
 	while (current != NULL)
 	{
