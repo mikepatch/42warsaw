650dc2ba246ec7b893159fbbe20d4c49afec1fce
Michal Lata <michallata.dev@gmail.com>
2024-12-28 04:55:11 +0100

    feat: Add ft_lstsize_bonus solution

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:39:35 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/28 04:39:54 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/28 04:53:53 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,7 @@ typedef struct s_list
 t_list	*ft_lstnew(void *content);
 
 void	ft_lstadd_front(t_list **lst, t_list *new);
+
+int		ft_lstsize(t_list *lst);
 
 #endif
