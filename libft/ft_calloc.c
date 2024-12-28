fa6c34c8b7432bc110e58ed7df41d875c038cefb
Michal Lata <michallata.dev@gmail.com>
2024-12-28 02:19:21 +0100

    update: Update ft_calloc implementation

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mlata <mlata@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 02:46:30 by mlata             #+#    #+#             */
-/*   Updated: 2024/12/26 05:43:49 by mlata            ###   ########.fr       */
+/*   Updated: 2024/12/28 02:12:57 by mlata            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	size_t	total_size;
-	char	*ptr;
+	void	*ptr;
 
 	total_size = count * size;
 	if (count != 0 && total_size / count != size)
 		return (NULL);
-	ptr = (char *)malloc(total_size);
+	ptr = malloc(total_size);
 	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, total_size);
